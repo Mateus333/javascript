ab676698196d5f50357b0a94fee062968f71723c
Mateus Sobreira <mateussobreira80@gmail.com>
2025-03-24 19:00:32 -0300

    Revisão da Aula 11 – Condições (Parte 1)

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,7 +1,8 @@
-var país = 'Brasil'
-console.log(`Vivendo em ${país}`)
-if (país != 'Brasil') {
-    console.log('Brasileiro!')
-}else {
-    console.log('Estrangeiro!')
-}
+// Criar esse site em forma de site, igual o ex008
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+} 
