9778f3b950332cd16e1843f599f63f8737261d4d
Mateus Sobreira <mateussobreira80@gmail.com>
2025-04-01 18:40:13 -0300

    Finalizando o Exercícios JavaScript (Parte 3)

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -2,39 +2,48 @@ function verificar () {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!!')
-    } else {
-        var fsex = document.getElementsByName('radsex') 
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERR0]Verifique os dados e tente novamente')
+    }else {
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var genero = ''
+        var gênero = ''
+        var img = document.createElement("img")
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
-            genero = 'Homem'
-            if (idade >=0 && idade < 10) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
                 img.setAttribute('src', 'homem-bebe.jpg')
-            } else if (idade < 21 ) {
+            }else if (idade < 21) {
+                //Jovem
                 img.setAttribute('src', 'homem-jovem.jpg')
-            } else if (idade < 50) {
+            }else if (idade < 50) {
+                //Adulto
                 img.setAttribute('src', 'homem-adulto.jpg')
-            } else {
+            }else {
+                //Idoso
                 img.setAttribute('src', 'homem-maduro.jpg')
             }
-        } else if (fsex[1].checked) {
-            genero = 'Feminino'
-            if (idade >=0 && idade < 10) {
+        }else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
                 //Criança
-            } else if (idade < 21 ) {
+                img.setAttribute('src', 'mulher-bebe.jpg')
+            }else if (idade < 21) {
                 //Jovem
-            } else if (idade < 50) {
+                img.setAttribute('src', 'mulher-jovem.jpg')
+            }else if (idade < 50) {
                 //Adulto
-            } else {
+                img.setAttribute('src', 'mulher-adulta.jpg')
+            }else {
                 //Idoso
+                img.setAttribute('src', 'mulher-madura.jpg')
             }
         }
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
         res.appendChild(img)
     }
-
 }
