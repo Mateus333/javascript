f74cfd95fda7bf13e4720cc2acfdc99f13e8f032
Mateus Sobreira <mateussobreira80@gmail.com>
2025-04-07 18:12:10 -0300

    Revisão da Aula 13 – Repetições (Parte 1)

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,26 +1,27 @@
 var c = 1
 do {
     console.log(`Passo ${c}`)
-    c++ // c = c + 1
-} while (c <= 29)
+    c++
+} while (c <= 6)
 
 
 /*
+MAIS ENCONTRADA DENTRO DOS PROGRAMAS
 
 var c = 1
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++ // c = c + 1
 }
-
+    
 */
 
 
 /*
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
 */
