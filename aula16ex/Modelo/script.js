132a17a5a53ebbc0b472cdab78c7396cabb4c1d3
Mateus Sobreira <121827360+Mateus333@users.noreply.github.com>
2025-05-13 18:00:44 -0300

    Exercícios JavaScript (Parte 8) Pt-2

--- aula16ex/Modelo/script.js
+++ aula16ex/Modelo/script.js
@@ -23,11 +23,11 @@ function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
+        item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
         res.innerHTML = ''
     } else {
-        window.alert('Valor inválido ou já encontrado na lista.')
+        window.alert(`Valor inválido ou já encontrado na lista.`)
     }
     num.value = ''
     num.focus()
@@ -38,9 +38,18 @@ function finalizar() {
         window.alert('Adicione valores antes de finalizar!')
     } else {
         let tot = valores.length
-
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
         res.innerHTML = ''
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
-
-    }
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        11:31
+    }   
 }
